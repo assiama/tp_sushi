fe63f3f49a9274aecec59ed355757f4ffdba83d3
Ibtissam Chtioui <Ibtissem.chti@hotmail.com>
2025-12-10 11:26:15 +0100

    Initial commit - Projet React Vite

--- src/main.jsx
+++ src/main.jsx
@@ -2,30 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { createBrowserRouter , BrowserRouter, RouterProvider} from 'react-router-dom'
 
-const router = createBrowserRouter(
-  [
-    {
-    path: "/",
-    element: <app/>,
-    children: [
-      {
-        path:"/",
-        element: <Acceuil/>,
-      },
-      {
-        path: "/menu",
-        element:<Menu/>,
-      }
-    ]
-  }
-  ]
-)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router = { router}/>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
